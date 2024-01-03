e1621eecc057445d2188c3caffb4a97b010ccda3
karlfroldan <karlfroldan@proton.me>
2024-01-03 23:46:35 +0800

    Updates:
    1. Remove nano. I prefer fine-grain control
    2. Custom org-mode stuff.
    3. Set keybindings for org-mode.

--- org/main.el
+++ org/main.el
@@ -1,6 +1,8 @@
-(defun my-org-mode-hook ()
-  (writer-mode))
-
-(defun bootstrap-org ()
-  (message "hello org"))
-;;  (add-hook 'org-mode-hook #'writer-mode))
+;; General org-mode settings
+(setq
+ ;; Use RET on keyboard to go to the specified link
+ org-return-follows-link t
+ ;; I want to see everything
+ org-startup-folded nil
+ ;; Use indentation for all org files
+ org-startup-indented t)
