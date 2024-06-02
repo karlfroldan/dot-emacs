c3bfe97c45af59a1fec83ee528562ba9a90df268
karlfroldan <karlfroldan@proton.me>
2024-06-02 21:54:27 +0800

    Change default haskell projects to stack

--- packages.el
+++ packages.el
@@ -89,7 +89,9 @@
 (use-package haskell-mode
   :ensure t
   :config
-  (custom-set-variables '(haskell-stylish-on-save t)))
+  (custom-set-variables '(haskell-stylish-on-save t))
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (setq haskell-process-type 'stack-ghci))
 
 (use-package lsp-haskell
   :ensure t
